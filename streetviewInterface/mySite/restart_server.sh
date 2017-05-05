0e618bfc971c7c438ec0831f4133c67c884efd64
michael <michael.chiang.mc5@gmail.com>
2017-05-04 22:20:29 -0700

    Added misc files

---
+++ streetviewInterface/mySite/restart_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python manage.py collectstatic
+pkill -f /home/michaelc/dev/slab-repo/venv/bin/gunicorn
+# python manage.py migrate --run-syncdb
+gunicorn mySite.wsgi --bind 127.0.0.1:8004 --daemon --log-file ~/dev/logs/slab.log --workers=1
