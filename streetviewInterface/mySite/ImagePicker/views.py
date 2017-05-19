400239c7b0fbc0c6c9d6bd5a59bf93bb6615d247
michael <michael.chiang.mc5@gmail.com>
2017-05-19 10:15:20 -0700

    Added basic image stitching

--- streetviewInterface/mySite/ImagePicker/views.py
+++ streetviewInterface/mySite/ImagePicker/views.py
@@ -9,6 +9,7 @@ import ast
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from PIL import Image
 
 def listImage(request):
     #return savePoint(request)
@@ -57,10 +58,101 @@ def savePoint(request):
     fov=22.5
     pitch=0
 
-    saveImage(xdim,ydim,latitude,longitude,fov,photographerHeading+90,pitch,mapPoint)
-    saveImage(xdim,ydim,latitude,longitude,fov,photographerHeading-90,pitch,mapPoint)
+    saveConcatImage(xdim,ydim,latitude,longitude,fov,photographerHeading+90,pitch,mapPoint)
+    saveConcatImage(xdim,ydim,latitude,longitude,fov,photographerHeading-90,pitch,mapPoint)
 
     return HttpResponse("done")
+
+def saveConcatImage(xdim,ydim,latitude,longitude,fov,heading,pitch,mapPoint):
+    saveImage2(xdim,ydim,latitude,longitude,fov,heading-fov,pitch,'temp1.jpg')
+    saveImage2(xdim,ydim,latitude,longitude,fov,heading    ,pitch,'temp2.jpg')
+    saveImage2(xdim,ydim,latitude,longitude,fov,heading+fov,pitch,'temp3.jpg')
+    I1 = Image.open(os.path.join(settings.MEDIA_ROOT,'temp1.jpg'))
+    I2 = Image.open(os.path.join(settings.MEDIA_ROOT,'temp2.jpg'))
+    I3 = Image.open(os.path.join(settings.MEDIA_ROOT,'temp3.jpg'))
+    # crop
+    I1 = I1.crop((0, 0, I1.size[0], I1.size[1]-20))
+    I2 = I2.crop((0, 0, I2.size[0], I2.size[1]-20))
+    I3 = I3.crop((0, 0, I3.size[0], I3.size[1]-20))
+
+    I_concatenate = concatenateImages([I2,I3])
+
+
+
+
+    # save StreetviewImage object
+    streetviewImage = StreetviewImage(mapPoint=mapPoint, \
+                                           heading=heading, \
+                                           fov=fov, \
+                                           pitch=pitch)
+    streetviewImage.save()
+    fi = str(streetviewImage.pk) + ".jpg"
+    fi_path = os.path.join(settings.MEDIA_ROOT,fi)
+    # save concatenated image
+    I_concatenate.save(fi_path)
+    #
+    streetviewImage.image = fi # this should be set with respect to MEDIA_ROOT
+    streetviewImage.save()
+
+def concatenateImages(I_array):
+    I_concatenate = I_array[0]
+    for i in range(1,len(I_array)):
+        I_concatenate = concatenateImage(I_concatenate,I_array[i])
+    return I_concatenate
+
+def concatenateImage(I_left, I_right):
+    # get column of right-most pixels for I_left and left-most pixels for I_right
+    column_left  = get_column(I_left.convert('L'),'last')
+    column_right = get_column(I_right.convert('L'),'first')
+    # find shift that minimizes distance
+    dimy = len(column_left)
+    shift_range = int(dimy/6) # decrease to speed up
+    min_average_distance = float("inf")
+    shift = 0
+    for i in range(-shift_range,shift_range):
+        sum_distance = 0
+        count = 0
+        for j in range(0,dimy):
+            if j+i<0 or j+i>=dimy:
+                continue
+            else:
+                v1 = column_left[j]
+                v2 = column_right[j+i]
+                distance = abs(v1-v2) ** 2
+                sum_distance += distance
+                count = count + 1
+        average_distance = sum_distance/count
+        if average_distance < min_average_distance:
+            min_average_distance=average_distance
+            shift=i
+    # shift right image to match left image
+    I_right_shift = Image.new('RGB', (I_right.size[0], I_right.size[1]))
+    I_right_shift.paste(I_right, (0,-shift))
+    # concatenate right and left image
+    dimy = max(I_left.size[1],I_right_shift.size[1])
+    dimx = I_left.size[0]+I_right_shift.size[0]
+    I_concatenate = Image.new('RGB', (dimx,dimy))
+    I_concatenate.paste(I_left, (0,0))
+    I_concatenate.paste(I_right_shift, (I_left.size[0],0))
+    return I_concatenate
+
+def get_column(I,firstOrLast):
+    pix = I.load()
+    dimx = I.size[0]
+    dimy = int(I.size[1])
+    column = [None]*dimy
+    for i in range(0,dimy):
+        if firstOrLast == 'first':
+            column[i] = pix[0,i]
+        elif firstOrLast == 'last':
+            column[i] = pix[dimx-1,i]
+    return column
+
+def saveImage2(xdim,ydim,latitude,longitude,fov,heading,pitch,filename):
+    url =   "http://maps.googleapis.com/maps/api/streetview?size=%dx%d&location=%f,%f&fov=%d&heading=%f&pitch=%f&key=AIzaSyBrwkUADkwqTvlC-HbKC_jZuqC3xBxUNLo" \
+                 % (xdim,ydim,latitude,longitude,fov,heading,pitch)
+    fi_path = os.path.join(settings.MEDIA_ROOT,filename)
+    data = urllib.request.urlretrieve(url, fi_path)
 
 def saveImage(xdim,ydim,latitude,longitude,fov,heading,pitch,mapPoint):
     url =   "http://maps.googleapis.com/maps/api/streetview?size=%dx%d&location=%f,%f&fov=%d&heading=%f&pitch=%f&key=AIzaSyBrwkUADkwqTvlC-HbKC_jZuqC3xBxUNLo" \
