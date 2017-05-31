37aa2e3b4700ac9730acba0edffd2612d8448639
michael <michael.chiang.mc5@gmail.com>
2017-05-31 01:46:49 -0700

    added admin checking for sensitive functions. deleteDB.sh now creates admin account user = admin, password = pass

--- streetviewInterface/mySite/deleteDB.sh
+++ streetviewInterface/mySite/deleteDB.sh
@@ -5,3 +5,5 @@ rm -rf ImagePicker/migrations
 rm media/*.jpg
 python manage.py makemigrations ImagePicker
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | ./manage.py shell
+
