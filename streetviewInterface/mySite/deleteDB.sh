4f34a6b65933168750983f6122b98ebfc08278e9
michael <michael.chiang.mc5@gmail.com>
2017-06-09 01:02:59 -0700

    admin account now created immediately

--- streetviewInterface/mySite/deleteDB.sh
+++ streetviewInterface/mySite/deleteDB.sh
@@ -5,5 +5,4 @@ rm -rf ImagePicker/migrations
 rm media/*.jpg
 python manage.py makemigrations ImagePicker
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | ./manage.py shell
-
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'asdf1234')" | ./manage.py shell
