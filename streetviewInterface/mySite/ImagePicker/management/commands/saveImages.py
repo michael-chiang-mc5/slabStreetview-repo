572b937f8c5c34531f95ec9c293e6ad911225abb
michael <michael.chiang.mc5@gmail.com>
2017-06-23 07:07:57 -0400

    exit thread after saveImage

--- streetviewInterface/mySite/ImagePicker/management/commands/saveImages.py
+++ streetviewInterface/mySite/ImagePicker/management/commands/saveImages.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from ImagePicker.views import saveImages_async
+import sys
 class Command(BaseCommand):
     help = 'saves images'
 
     def handle(self, *args, **options):
         saveImages_async()
+        sys.exit(0)
