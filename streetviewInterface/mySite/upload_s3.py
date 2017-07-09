5922a1e63ff976eef9f0926c91021580d15f939d
Michael Chiang <michaelchiang@Michaels-iMac.local>
2017-07-08 17:19:45 -0700

    added method to upload images to s3

--- streetviewInterface/mySite/upload_s3.py
+++ streetviewInterface/mySite/upload_s3.py
@@ -2,14 +2,19 @@ import boto3
 from mySite.settings import *
 import sys
 
+# Example url:
+# https://s3-us-west-1.amazonaws.com/slab-streetview/1.jpg
+
 def main():
     # print command line arguments
-    for arg in sys.argv[1:]:
-        print(arg)
-    print(AWS_ACCESS_KEY)
-    print(AWS_SECRET)
+    start_pk = int(sys.argv[1])
+    end_pk = int(sys.argv[2])
+    for pk in range(start_pk,end_pk+1):
+        image_name = str(pk) + '.jpg'
+        image_path = 'media/' + image_name
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET,)
+        s3.upload_file(image_path, 'slab-streetview', image_name)
+        print(image_path + ' uploaded to s3 as https://s3-us-west-1.amazonaws.com/slab-streetview/' + image_name)
 
 if __name__ == "__main__":
     main()
-
-
