07766aff24dea05de080e7abb8471aba93b2e1ed
michael <michael.chiang.mc5@gmail.com>
2017-07-19 01:56:27 -0700

    added missing files

---
+++ streetviewInterface/mySite/ImagePicker/views_figures.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+import os
+import urllib.request
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import ast
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from PIL import Image
+from math import sqrt
+from collections import Counter, defaultdict
+from .google_ocr import *
+import time
+import sys
+import subprocess
+from random import randint
+import json
+import csv
+import boto3
+from random import randint
+from time import sleep
+
+def index_figures(request):
+    context = {}
+    return render(request, 'ImagePicker/index_figures.html',context)
