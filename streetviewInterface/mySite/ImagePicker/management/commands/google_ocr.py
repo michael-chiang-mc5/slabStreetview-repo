3d3d828c67a18b64bd442adae97d8a5501a7e595
michael <michael.chiang.mc5@gmail.com>
2018-02-28 22:56:18 -0800

    minor fix

--- streetviewInterface/mySite/ImagePicker/management/commands/google_ocr.py
+++ streetviewInterface/mySite/ImagePicker/management/commands/google_ocr.py
@@ -2,12 +2,17 @@ from django.core.management.base import BaseCommand, CommandError
 from ImagePicker.views import run_google_ocr
 import sys
 class Command(BaseCommand):
-    help = 'saves mapPoint text file'
+    help = 'python manage.py google_ocr maxCalls priority(1=True)'
 
     def add_arguments(self, parser):
-        parser.add_argument('max-api-calls', nargs=1, type=int)
+        parser.add_argument('max-api-calls', nargs=2, type=int)
 
     def handle(self, *args, **options):
         max_api_calls = options['max-api-calls'][0]
-        run_google_ocr(max_api_calls)
+        priority = options['max-api-calls'][1]
+        if priority is 0:
+            priority = False
+        else:
+            priority = True
+        run_google_ocr(max_api_calls,priority)
         sys.exit(0)
