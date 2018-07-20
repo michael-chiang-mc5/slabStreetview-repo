9343468958eb9e3dc8ada3d33f587758b6275b38
michael <michael.chiang.mc5@gmail.com>
2018-07-20 16:14:19 -0700

    minor fix

--- streetviewInterface/mySite/ImagePicker/management/commands/createImages.r
+++ streetviewInterface/mySite/ImagePicker/management/commands/createImages.r
@@ -2,7 +2,7 @@
 library(ggmap)
 
 # read csv file
-MyData <- read.csv(file="../streetviewInterface/mySite/media/MapPoint.csv", header=TRUE, sep="\t",stringsAsFactors = FALSE)
+MyData <- read.csv(file="../../../media/MapPoint.csv", header=TRUE, sep="\t",stringsAsFactors = FALSE)
 lon=MyData[['longitude']]
 lat=MyData[['latitude']]
 
@@ -10,7 +10,7 @@ lat=MyData[['latitude']]
 map <- get_map(location=c(lon=-118.17687109859778, lat=34.00464406169281), zoom = 11, color = "bw", scale = 2, maptype = "roadmap")
 
 # plot number of signs on map
-png(filename="output/total_mapPoints.png", width=1280, height=1280)
+png(filename="../../../media/MapPoint.png", width=1280, height=1280)
 # See http://ggplot2.tidyverse.org/reference/geom_point.html for same aesthetic options
 # Also see: https://rpubs.com/jiayiliu/ggmap_examples
 df <- data.frame(lon=lon, lat=lat)
