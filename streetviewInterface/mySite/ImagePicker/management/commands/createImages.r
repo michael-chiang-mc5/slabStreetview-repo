fd856d12a449bbca8d03be93561837830540e15c
michael <michael.chiang.mc5@gmail.com>
2018-07-20 18:02:48 -0700

    fixes

--- streetviewInterface/mySite/ImagePicker/management/commands/createImages.r
+++ streetviewInterface/mySite/ImagePicker/management/commands/createImages.r
@@ -11,20 +11,33 @@ library('withr',lib.loc='Rpackages')
 library('ggplot2',lib.loc='Rpackages')
 library('ggmap',lib.loc='Rpackages')
 library('labeling',lib.loc='Rpackages')
+library('ggmap')
+map <- get_map(location=c(lon=-118.17687109859778, lat=34.00464406169281), zoom = 11, color = "bw", scale = 2, maptype = "roadmap")
+## ggplot is stupid and won't work in a for loop
 
-
-# read csv file
+# Sign.png
 MyData <- read.csv(file="../../../media/Sign.csv", header=TRUE, sep="\t",stringsAsFactors = FALSE)
 lon=MyData[['longitude']]
 lat=MyData[['latitude']]
-
-# Choose between stamen map and google map
-map <- get_map(location=c(lon=-118.17687109859778, lat=34.00464406169281), zoom = 11, color = "bw", scale = 2, maptype = "roadmap")
-
-# plot number of signs on map
 png(filename="../../../media/Sign.png", width=1280, height=1280)
-# See http://ggplot2.tidyverse.org/reference/geom_point.html for same aesthetic options
-# Also see: https://rpubs.com/jiayiliu/ggmap_examples
+df <- data.frame(lon=lon, lat=lat)
+ggmap(map) + geom_point(aes(x=lon,y=lat),data=df) + geom_point(size=3,alpha=0.3)
+dev.off()
+
+# GoogleOCR.png
+MyData <- read.csv(file="../../../media/GoogleOCR.csv", header=TRUE, sep="\t",stringsAsFactors = FALSE)
+lon=MyData[['longitude']]
+lat=MyData[['latitude']]
+png(filename="../../../media/GoogleOCR.png", width=1280, height=1280)
+df <- data.frame(lon=lon, lat=lat)
+ggmap(map) + geom_point(aes(x=lon,y=lat),data=df) + geom_point(size=3,alpha=0.3)
+dev.off()
+
+# MapPoint.png
+MyData <- read.csv(file="../../../media/MapPoint.csv", header=TRUE, sep="\t",stringsAsFactors = FALSE)
+lon=MyData[['longitude']]
+lat=MyData[['latitude']]
+png(filename="../../../media/MapPoint.png", width=1280, height=1280)
 df <- data.frame(lon=lon, lat=lat)
 ggmap(map) + geom_point(aes(x=lon,y=lat),data=df) + geom_point(size=3,alpha=0.3)
 dev.off()
