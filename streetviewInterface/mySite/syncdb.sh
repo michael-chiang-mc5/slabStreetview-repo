97757ae87ee9875ed81b24df7feff72a7e138c44
Michael Chiang <michael.chiang.mc5@gmail.com>
2018-10-25 11:56:34 -0700

    Minor changes

--- streetviewInterface/mySite/syncdb.sh
+++ streetviewInterface/mySite/syncdb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 rm -rf ImagePicker/migrations
-scp -r michaelc@104.131.145.75:/home/michaelc/dev/slabStreetview-repo/streetviewInterface/mySite/ImagePicker/migrations ImagePicker/migrations
+scp -r michaelc@138.197.220.71:/home/michaelc/dev/slabStreetview-repo/streetviewInterface/mySite/ImagePicker/migrations ImagePicker/migrations
 
-
-scp michaelc@104.131.145.75:/home/michaelc/dev/slabStreetview-repo/streetviewInterface/mySite/db.sqlite3 db.sqlite3
+scp michaelc@138.197.220.71:/home/michaelc/dev/slabStreetview-repo/streetviewInterface/mySite/db.sqlite3 db.sqlite3
 
